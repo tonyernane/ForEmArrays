68d2d434b5d766f78104fc4e87a0ca1db04bda27
Otoniel <tonyernane@gmail.com>
2024-09-02 13:04:25 -0300

    first commit

---
+++ src/ForEmArrays.java
@@ -0,0 +1,8 @@
+public class ForEmArrays {
+public static void main(String[] args) {
+    String membros[] = {"Mateus", "Marcos", "Lucas", "João", "André"};
+    for (int x =0; x < membros.length; x++){
+        System.out.println("o irmão na posição x=" + x + " é o irmão " + membros [ x ] );
+    }
+}
+}
